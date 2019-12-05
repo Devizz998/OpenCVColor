c4e29e1d49b0db3ba6b3f350680c353b07cfad0c
Devizz998 <mattiadevivo@gmail.com>
2019-12-05 10:56:51 +0100

    Update ColorBlobDetector.java
    Tollerance incremented to detect different color gradation

--- app/src/main/java/com/example/opencvproject/ColorBlobDetector.java
+++ app/src/main/java/com/example/opencvproject/ColorBlobDetector.java
@@ -21,7 +21,8 @@ public class ColorBlobDetector {
     // Minimum contour area in percent for contours filtering
     private static double mMinContourArea = 0.1;
     // Color radius for range checking in HSV color space
-    private Scalar mColorRadius = new Scalar(25,50,50,0);
+    // Initial value, more accurate (25, 50, 50)
+    private Scalar mColorRadius = new Scalar(25,50,75,0);
     private Mat mSpectrum = new Mat();
     private List<MatOfPoint> mContours = new ArrayList<MatOfPoint>();
     
